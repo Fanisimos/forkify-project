707d4120560ea50b9d3172c3da5c9ebbc106ae8f
fanisimos <xspot1995@hotmail.com>
2024-08-01 18:18:29 +0100

    master

--- src/js/helper.js
+++ src/js/helper.js
@@ -60,35 +60,3 @@ export const numberToFraction = function (amount) {
   }
   return amount;
 };
-
-//Refactor the getJSON & sendJSON functions into a single function
-
-// export const getJSON = async function (url) {
-//   try {
-//     const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
-//     const data = await res.json();
-//     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-//     return data;
-//   } catch (err) {
-//     throw err;
-//   }
-// };
-
-// export const sendJSON = async function (url, uploadData) {
-//   try {
-//     const fetchPro = fetch(url, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(uploadData),
-//     });
-//     const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
-//     const data = await res.json();
-
-//     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-//     return data;
-//   } catch (err) {
-//     throw err;
-//   }
-// };
