646bec0af67fd61717243b76c9f6f5ed3571d47b
fanisimos <xspot1995@hotmail.com>
2024-08-01 16:22:49 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b8377b59-84e4-4eee-9bc6-bca7ecd21722';
+export const CLOSE_MODAL_SEC = 2.5;
