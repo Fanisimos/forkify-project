640f790f0c8937ec23bdba5fd8882797f4ad598f
fanisimos <xspot1995@hotmail.com>
2024-08-01 17:39:19 +0100

    Fix bugs

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@
 
 import View from './view';
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+import { Fraction } from 'fractional/index';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
